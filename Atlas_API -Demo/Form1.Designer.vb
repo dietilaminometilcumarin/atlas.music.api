c7c3a82c44ba1cbe1a2333cf9367b24b5b9b1c7e
Site <68510239+dietilaminometilcumarin@users.noreply.github.com>
2024-03-02 17:01:44 +0200

    Add files via upload

---
+++ Atlas_API -Demo/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(535, 498)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(152, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(24, 49)
+        Me.ListView1.MultiSelect = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.ShowGroups = False
+        Me.ListView1.Size = New System.Drawing.Size(663, 419)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        Me.ColumnHeader1.Width = 70
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Track Name"
+        Me.ColumnHeader2.Width = 138
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Artist Name"
+        Me.ColumnHeader3.Width = 193
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Genre"
+        Me.ColumnHeader5.Width = 150
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Date/Time"
+        Me.ColumnHeader6.Width = 75
+        '
+        'ListView2
+        '
+        Me.ListView2.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader4, Me.ColumnHeader7})
+        Me.ListView2.FullRowSelect = True
+        Me.ListView2.Location = New System.Drawing.Point(693, 49)
+        Me.ListView2.MultiSelect = False
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.ShowGroups = False
+        Me.ListView2.Size = New System.Drawing.Size(185, 419)
+        Me.ListView2.TabIndex = 4
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "ID"
+        Me.ColumnHeader4.Width = 50
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Play List"
+        Me.ColumnHeader7.Width = 135
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(693, 498)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(185, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Load Play Lists"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(905, 549)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Atlas.Music [atlas.callback@gmail.com]"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader7 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ Atlas_API -Demo/Form1.vb
@@ -0,0 +1,82 @@
+﻿
+'Demo application for demonstrate using of Atlas_API component
+'Copyright 2024, Detach.pp.ua (atlas.callback@gmail.com)
+
+'-------------------------------------------------------
+'MAIN API 
+
+'PlayList.................structure, describes a description of the playlist type
+'ListMusicData............structure, describes the attributes of a music track
+
+
+'ConnectToServer..........remote host connection function
+'GetPlayLists.............playlist list function (return type - PlayList)
+'GetTracksByPlayListID....function for obtaining a list of tracks for a specified playlist (return type - ListMusicData)
+'Player_EndTrack..........current playback end event
+'-------------------------------------------------------
+
+
+Imports Atlas_API
+
+Public Class Form1
+
+    'THE COMPONENT CONTAINS SOME EVENTS SO LET'S CONNECT USING - WithEvents
+    Public WithEvents TCPC As Atlas_Music_API = Atlas_Music_API.Instance
+
+    'INITIALIZING CONNECTION TO THE SERVER
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If TCPC.ConnectToServer("TOKEN") Then
+            Beep()
+        Else
+            MsgBox("Error Connection")
+            End
+        End If
+    End Sub
+
+    'STOPPING CURRENT PLAYBACK
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TCPC.StopSong()
+    End Sub
+
+    'PLAYING A TRACK BY CURRENT ID
+    Private Sub ListView1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListView1.MouseDoubleClick
+        If ListView1.SelectedItems.Count > 0 Then
+            TCPC.PlaySong(ListView1.SelectedItems(0).Text)
+        End If
+    End Sub
+
+    'EVENT NOTIFYING THE COMPLETION OF PLAYING THE SELECTED TRACK
+    Private Sub TCPC_Player_EndTrack() Handles TCPC.Player_EndTrack
+        If ListView1.SelectedItems.Count > 0 And ListView1.SelectedItems(0).Index < ListView1.Items.Count - 1 Then
+            ListView1.Items(ListView1.SelectedItems(0).Index + 1).Selected = True
+            TCPC.PlaySong(ListView1.SelectedItems(0).Text)
+        End If
+    End Sub
+
+    'RETURN A SET OF PLAYLISTS STORED ON A REMOTE SERVICE
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListView2.Items.Clear()
+        Dim pl As List(Of Atlas_Music_API.PlayList) = TCPC.GetPlayLists()
+
+        For Each item As Atlas_Music_API.PlayList In pl
+            Dim lItem As ListViewItem = ListView2.Items.Add(item.ListID)
+            lItem.SubItems.Add(item.ListName)
+        Next
+    End Sub
+
+    'RETURN A LIST OF TRACKS AT THE SPECIFIED PLAYLIST ID
+    Private Sub ListView2_DoubleClick(sender As Object, e As EventArgs) Handles ListView2.DoubleClick
+        ListView1.Items.Clear()
+        Dim playList As List(Of Atlas_Music_API.ListMusicData) = TCPC.GetTracksByPlayListID(ListView2.SelectedItems(0).Text)
+        ListView1.Items.Clear()
+        For Each item As Atlas_Music_API.ListMusicData In playList
+            Dim lItem As ListViewItem = ListView1.Items.Add(item.ID)
+            lItem.SubItems.Add(item.TrackName)
+            lItem.SubItems.Add(item.ArtistName)
+            lItem.SubItems.Add(item.TrackGenre)
+            lItem.SubItems.Add(item.TrackDuration)
+        Next
+    End Sub
+
+
+End Class
